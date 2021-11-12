017074412c7a28fd856f10c5f5deaffcb72a873c
Michael Fürstenberg <michael@furstenberg.dev>
2021-11-12 08:57:32 +0100

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+/** @type {import('@sveltejs/kit').Config} */
+
+import vercel from '@sveltejs/adapter-vercel';
+
+const config = {
+    kit: {
+        adapter: vercel(),
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte'
+    }
+};
+
+export default config;
